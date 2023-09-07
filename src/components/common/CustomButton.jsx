0f19c809c7e60517b606b2512f7104aece4ec147
Tusharjagi <tusharjagi@gmail.com>
2023-09-07 14:57:54 +0530

    update custom button

--- src/components/common/CustomButton.jsx
+++ src/components/common/CustomButton.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Button } from "@mui/joy";
 
-const CustomButton = ({ color, size, variant, text }) => {
+const CustomButton = ({ color, size, variant, text, onClick }) => {
   return (
-    <Button color={color} size={size} variant={variant}>
+    <Button color={color} size={size} variant={variant} onClick={onClick}>
       {text}
     </Button>
   );
